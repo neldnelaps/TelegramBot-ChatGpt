fa895343ff3d5ec4f193e727494fb97b24c9af2e
nelaps <neldnelaps@gmail.com>
2023-04-11 13:02:18 +0700

    Update main.py

--- ChatGPT/main.py
+++ ChatGPT/main.py
@@ -17,7 +17,7 @@ async def send (message: types.Message):
     model="text-davinci-003",
     prompt=message.text,
     temperature=0.9,
-    max_tokens=150,
+    max_tokens=1000,
     top_p=1,
     frequency_penalty=0.0,
     presence_penalty=0.6,
@@ -25,5 +25,4 @@ async def send (message: types.Message):
 )
     await message.answer(response['choices'][0]['text'])
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+executor.start_polling(dp, skip_updates=True)
